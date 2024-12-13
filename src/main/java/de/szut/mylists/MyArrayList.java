3eadf949c35b14e96781d8e4703731429cb9efb3
Marius Meschter <git@meschter.me>
2024-12-13 10:55:39 +0100

    lösung

--- src/main/java/de/szut/mylists/MyArrayList.java
+++ src/main/java/de/szut/mylists/MyArrayList.java
@@ -1,5 +1,64 @@
 package de.szut.mylists;
 
 public class MyArrayList {
+    private int[] storage = new int[10];
+    private int elements = 0;
+    private int capacity = 10;
 
+    public int size() {
+        return elements;
+    }
+
+    private void growStorage() {
+        var oldCapacity = capacity;
+        capacity += 10;
+        var newList = new int[capacity];
+        System.arraycopy(storage, 0, newList, 0, oldCapacity);
+        storage = newList;
+    }
+
+    public void add(int value) {
+        if (elements >= capacity) {
+            growStorage();
+        }
+
+        storage[elements] = value;
+        elements += 1;
+    }
+
+    public int get(int index) {
+        boundsCheck(index);
+        return storage[index];
+    }
+
+    public void remove(int index) {
+        boundsCheck(index);
+        // copy all elements starting at index + 1 to index
+        // basically shifting everything bigger than index one to the left
+
+        // elements remaining after the index
+        // len = 5, index = 3
+        // remaining = 5 - (3 + 1) = 1
+        // meaning after index 3 there is one more element in the array we have to copy
+        int remaining = storage.length - (index + 1);
+
+        System.arraycopy(storage, index + 1, storage, index, remaining);
+        elements -= 1;
+    }
+
+    private void boundsCheck(int index) {
+        if (index > elements) {
+            throw new IndexOutOfBoundsException("Dieser Index existiert nicht!");
+        }
+    }
+
+    public boolean contains(int number) {
+        for (int i = 0; i < elements; i++) {
+            if (storage[i] == number) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/test/java/de/szut/mylists/MyArrayListTest.java
+++ src/test/java/de/szut/mylists/MyArrayListTest.java
@@ -1,5 +1,148 @@
 package de.szut.mylists;
 
-public class MyArrayListTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MyArrayListTest {
+    MyArrayList list;
+
+    void fillListWithItems(int n) {
+        for (int i = 0; i < n; i++) {
+            list.add(i + 1);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        this.list = new MyArrayList();
+    }
+
+    @Test
+    void givenEmptyList_whenSize_returnZero() {
+        var size = list.size();
+
+        assertThat(size).isEqualTo(0);
+    }
+
+    @Test
+    void givenListWithOneItem_whenSize_returnOne() {
+        list.add(1);
+        var size = list.size();
+
+        assertThat(size).isEqualTo(1);
+    }
+
+    @Test
+    void givenListWithTwoItems_whenSize_returnTwo() {
+        fillListWithItems(2);
+
+        var size = list.size();
+
+        assertThat(size).isEqualTo(2);
+    }
+
+    @Test
+    void givenListWithOneItem_whenGet0_returnCorrectItem() {
+        list.add(1);
+
+        var item = list.get(0);
+
+        assertThat(item).isEqualTo(1);
+    }
+
+    @Test
+    void givenListWithTwoItems_whenGet1_returnCorrectItem() {
+        fillListWithItems(2);
+
+        var item = list.get(1);
+
+        assertThat(item).isEqualTo(2);
+    }
+
+    @Test
+    void givenListWithTwoItems_whenGet3_throwOutOfBoundsException() {
+        fillListWithItems(2);
+
+        assertThatThrownBy(() -> {
+            list.get(3);
+        }).isInstanceOf(RuntimeException.class).hasMessage("Dieser Index existiert nicht!");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 8, 10, 15, 25, 35, 1000, 10000})
+    void givenNItems_whenGetN_returnCorrectLastItem(int n) {
+        fillListWithItems(n);
+
+        var item = list.get(n - 1);
+
+        assertThat(item).isEqualTo(n);
+    }
+
+    @Test
+    void givenListWithOneItem_whenRemove_returnSize0() {
+        fillListWithItems(1);
+
+        list.remove(0);
+
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    void givenListWithTwoItems_whenRemoveItem2_returnSize1() {
+        fillListWithItems(2);
+
+        list.remove(1);
+
+        assertThat(list.size()).isEqualTo(1);
+    }
+
+    @Test
+    void givenListWithTwoItems_whenRemoveItem1AndGetIndex0_return2() {
+        fillListWithItems(2);
+
+        list.remove(0);
+
+        assertThat(list.get(0)).isEqualTo(2);
+    }
+
+    @Test
+    void givenListWithFiveItems_whenRemoveIndex3AndGetIndex3_return5() {
+        fillListWithItems(5);
+
+        list.remove(3);
+
+        assertThat(list.get(3)).isEqualTo(5);
+    }
+
+    @Test
+    void givenListWithFiveItems_whenRemoveIndexSix_returnException() {
+        fillListWithItems(5);
+
+        assertThatThrownBy(() -> {
+            list.remove(6);
+        }).isInstanceOf(RuntimeException.class).hasMessage("Dieser Index existiert nicht!");
+    }
+
+    @Test
+    void givenListWithFiveItems_whenContains3_returnTrue() {
+        fillListWithItems(5);
+
+        var result = list.contains(3);
+
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void givenListWithFiveItems_whenContains6_returnFalse() {
+        fillListWithItems(5);
+
+        var result = list.contains(6);
+
+        assertThat(result).isFalse();
+    }
 }
